ad792e0ce26ee05497741eef5d4d0d49995c19d0
Yashodhan Ketkar <kykyashodhan@gmail.com>
2024-01-16 17:18:46 +0530

    updates
    1. Added game module to seperate game logic
    2. Seperated guess validation logic from core logic
    3. Added vscode directory to gitignore

---
+++ src/game/mod.rs
@@ -0,0 +1,5 @@
+pub mod core;
+pub mod validation;
+
+pub use core::run;
+pub use validation::validate_guess;
---
+++ src/game/validation.rs
@@ -0,0 +1,73 @@
+/// This function validate user choise and compare it to secrete number
+///
+/// # Arguments
+///
+/// * `guess` - user choise
+/// * `secret_number` - secrete number
+/// * `tries` - number of tries
+///
+/// # Returns
+///
+/// * `bool` - true if guess is correct, false otherwise
+///
+/// # Examples
+/// 1. Correct guess
+/// ```rust
+/// let guess = 10;
+/// let secret_number = 10;
+/// let tries = 10;
+///
+/// validate_guess(guess, secret_number, tries);
+///
+/// // Output:
+/// // Correct guess: 10
+/// ```
+///
+/// 2. Incorrect close but lower guess
+/// ```rust
+/// let guess = 16;
+/// let secret_number = 20;
+/// let tries = 10;
+///
+/// validate_guess(guess, secret_number, tries);
+///
+/// // Output:
+/// // Close guess, but go higher
+/// ```
+/// 3. Incorrect higher guess
+/// ```rust
+/// let guess = 20;
+/// let secret_number = 10;
+/// let tries = 10;
+///
+/// validate_guess(guess, secret_number, tries);
+///
+/// // Output:
+/// // Go much lower
+/// ```
+///
+pub fn validate_guess(guess: i32, secret_number: i32, tries: i32) -> bool {
+    if guess == secret_number {
+        println!("Correct guess: {guess}");
+        return true;
+    }
+
+    if tries == 1 {
+        return false;
+    }
+
+    if guess < secret_number {
+        if guess + 5 > secret_number {
+            println!("Close guess, but go higher")
+        } else {
+            println!("Go much higher");
+        }
+    } else if guess > secret_number {
+        if guess - 5 < secret_number {
+            println!("Close guess, but go lower")
+        } else {
+            println!("Go much lower");
+        }
+    }
+    return false;
+}
